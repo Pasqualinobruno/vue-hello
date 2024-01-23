afbabdab959fc8eeef91d4071bbb144bf6f71211
Pasqualibruno <pasqualinomolosso@gmail.com>
2024-01-23 15:19:50 +0100

    vue-hello

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'hello world',
+      Image: ''
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <img src="https://i.pinimg.com/736x/bf/5c/ed/bf5cedf542c2cf6f0fa1eb714ad33455.jpg" alt="">
+</template>
+
+<style scoped></style>
